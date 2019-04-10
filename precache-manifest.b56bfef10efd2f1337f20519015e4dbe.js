91d5877929c97e9e938d1c06a777c8ef532906d7
Arnab Ghosh <44019889+arnabghs@users.noreply.github.com>
2019-04-10 10:52:37 +0530

    Updates

---
+++ precache-manifest.b56bfef10efd2f1337f20519015e4dbe.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "5a3fc8d14b65c7970df8",
+    "url": "/game_of_life/static/js/runtime~main.7abc2d87.js"
+  },
+  {
+    "revision": "d73c012487f03f64ffd1",
+    "url": "/game_of_life/static/js/main.80cf9e62.chunk.js"
+  },
+  {
+    "revision": "58f496bc1ce27350f0eb",
+    "url": "/game_of_life/static/js/2.98dcc9dd.chunk.js"
+  },
+  {
+    "revision": "d73c012487f03f64ffd1",
+    "url": "/game_of_life/static/css/main.dcc1cd93.chunk.css"
+  },
+  {
+    "revision": "c712fb169b7836df9434bb024fd29f23",
+    "url": "/game_of_life/index.html"
+  }
+];
